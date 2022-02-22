d7d7355bf1e4bf421ad2f1b868e2165b92c3f122
aid <aisharahm2101@gmail.com>
2022-02-22 17:37:19 +0300

    Init

---
+++ app/src/main/java/com/example/aid/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.aid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /////ааааа умираюю
+    }
+}
